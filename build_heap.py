06b9bf21d981797034ab8b10e7d9d2b190882063
0931REB462 <125568233+0931REB462@users.noreply.github.com>
2023-03-06 11:24:35 +0000

    ad

--- build_heap.py
+++ build_heap.py
@@ -1,41 +1,42 @@
-# python3
 
 
-def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+class MinHeap:
+    def __init__(self, array):
+        self.A = array
+        self.size = len(self.A)
+        self.swaps = []
 
 
-    return swaps
+    def SiftDown(self, i):
+        min_index = i
+        left = 2 * i + 1  # left child
+        right = 2 * i + 2  # right child
+        if left < self.size and self.A[left] < self.A[min_index]:
+            min_index = left
+        if right < self.size and self.A[right] < self.A[min_index]:
+            min_index = right
+        if min_index != i:
+            self.swaps.append((i, min_index))
+            self.A[i], self.A[min_index] = self.A[min_index], self.A[i]
+            self.SiftDown(min_index)
+
+    def BuildHeap(self):
+        n = self.size
+        for i in range(n // 2 - 1, -1, -1):
+            self.SiftDown(i)
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
-    data = list(map(int, input().split()))
+    array = list(map(int, input().split()))
+    assert len(array) == n
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
+    heap = MinHeap(array)
+    MinHeap.BuildHeap(heap)
+    swaps = heap.swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    for swap in swaps:
+        print(*swap)
 
 
 if __name__ == "__main__":
